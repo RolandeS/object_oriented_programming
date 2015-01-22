a26912a388f673de445e9c51539db11efb39f9b3
Rolande Saliba <rolande.saliba@gmail.com>
2015-01-22 15:09:54 -0500

    Step 5: greeting

--- people.rb
+++ people.rb
@@ -4,10 +4,18 @@ class Person
 	def initialize(name)
 		@name = name
 	end
+# number 5: The best place to put this method is in the class Person:
+	def greet(name)
+		puts "Hi, my name is #{name}"
+	end
 end
 
-class Student
+
+#CLASS STUDENT
+class Student < Person
+
 	def initialize
+		super(name)
 	end
 
 	def learn
@@ -15,6 +23,7 @@ class Student
 	end
 end
 
+#CLASS INSTRUCTOR
 class Instructor
 	def initialize
 	end
@@ -29,3 +38,4 @@ end
 # Testing purposes
 test = Student.new.learn
 test2 = Instructor.new.teach
+test3 = Student.new.greet("Ro")
