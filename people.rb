77c570a5e0b0bf425aa58cdb44c16b7da123bd9d
Rolande Saliba <rolande.saliba@gmail.com>
2015-01-22 14:43:40 -0500

    Here we have already the first 3 steps done (last one is the method teach)

--- people.rb
+++ people.rb
@@ -1,9 +1,23 @@
 class Student
 	def initialize
 	end
+
+	def learn
+		puts "I get it!"
+	end
 end
 
 class Instructor
 	def initialize
 	end
+
+	def teach
+		puts "Everything in Ruby is an Object"
+	end
 end
+
+
+
+# Testing purposes
+test = Student.new.learn
+test2 = Instructor.new.teach
